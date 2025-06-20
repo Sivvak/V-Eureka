f126118cc396dbda66b4098a5eee07fae3ba714e
Sivvak <pawel.7p@gmail.com>
2025-06-20 10:07:40 +0200

    adjust default config

--- genesisgymenvs/genesisgymenvs/train.py
+++ genesisgymenvs/genesisgymenvs/train.py
@@ -146,7 +146,7 @@ def get_cfgs():
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--exp_name", type=str, default="go2-walking")
-    parser.add_argument("-B", "--num_envs", type=int, default=2)
+    parser.add_argument("-B", "--num_envs", type=int, default=512)
     parser.add_argument("--max_iterations", type=int, default=2)
     args = parser.parse_args()
 
